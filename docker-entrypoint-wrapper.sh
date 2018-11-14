d7b3305b037bed1ea2ce70fb7ac6c22401c86cb9
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2018-11-14 12:33:22 +1100

    Set memlock limit

--- docker-entrypoint-wrapper.sh
+++ docker-entrypoint-wrapper.sh
@@ -4,4 +4,6 @@ source /root/bootstrap/*
 
 gomplate --file /usr/share/elasticsearch/config/elasticsearch.yml.gotpl --out /usr/share/elasticsearch/config/elasticsearch.yml
 
+ulimit -l unlimited
+
 exec /usr/local/bin/docker-entrypoint.sh $@
